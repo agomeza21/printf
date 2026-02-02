ead7f80a624d308a3441324198f2e34981ecba19
Alejandro Gomez Andres <agomez-a@c3r3s5.42urduliz.com>
2026-02-02 13:03:26 +0100

    Proyecto printf terminado.

--- ft_printf.h
+++ ft_printf.h
@@ -6,25 +6,25 @@
 /*   By: agomez-a <agomez-a@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 11:08:57 by agomez-a          #+#    #+#             */
-/*   Updated: 2026/01/30 19:45:55 by agomez-a         ###   ########.fr       */
+/*   Updated: 2026/02/02 10:11:20 by agomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdint.h>
 # include <unistd.h>
 
 void	ft_putchar(char c);
-int	    text_conversion(char *especifiers, va_list *args);
-int	    format_especifiers(const char *str, va_list *args);
-int	    ft_putstr(char *str);
-int	    ft_putnbr(int n);
-int	    ft_unsigned(unsigned int n);
-int	    ft_hex_low(unsigned int num);
-int	    ft_hex_up(unsigned int num);
-int	    ft_pointer(void *ptr);
+int		text_conversion(char *especifiers, va_list *args);
+int		format_especifiers(const char *str, va_list *args);
+int		ft_putstr(char *str);
+int		ft_putnbr(int n);
+int		ft_unsigned(unsigned int n);
+int		ft_hex_low(unsigned int num);
+int		ft_hex_up(unsigned int num);
+int		ft_pointer(void *ptr);
 
 #endif
